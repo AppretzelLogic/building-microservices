190cc2208e0f580ff1f9bba288d01a2fd9fa9697
Andrew L Schaeferle <andyschaeferle@gmail.com>
2023-05-13 17:53:09 -0400

    Finnish go sec 4

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -16,6 +17,11 @@ type Product struct {
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
 	DeletedOn   string  `json:"-"`
+}
+
+func (p *Product) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(p)
 }
 
 // Products is a collection of Product
@@ -37,9 +43,40 @@ func GetProducts() Products {
 	return productList
 }
 
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
+}
+
 // productList is a hard coded list of products for this example data source
 // example data source
-var productList = []*Product{	
+var productList = []*Product{
 	&Product{
 		ID:          1,
 		Name:        "Latte",
@@ -48,7 +85,7 @@ var productList = []*Product{
 		SKU:         "abc323",
 		CreatedOn:   time.Now().UTC().String(),
 		UpdatedOn:   time.Now().UTC().String(),
-},
+	},
 	&Product{
 		ID:          2,
 		Name:        "Espresso",
--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -1,42 +1,27 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
 	"github.com/AppretzelLogic/go-microservices/product-api/data"
+	"github.com/gorilla/mux"
 )
 
 // Products is a http.Handler
-type Products struct {
-	l *log.Logger
-}
+type ProductServiceHandler struct {}
 
 // NewProducts creates a products handler with the given logger
-func NewProducts(l *log.Logger) *Products {
-	return &Products{l}
-}
-
-// ServeHttp is the main entry point for the handler and satisfies the http.Handler interface
-// interface
-func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	// handle GET
-	if r.Method == http.MethodGet {
-		p.getProducts(rw, r)
-		return
+func (ph *ProductServiceHandler) NewProductServiceHandler(w http.ResponseWriter, r *http.Request) {
+	// fetch the products from the datastore
+	lp := data.GetProducts()
+	// serialize the list to JSON
+	err := lp.ToJSON(w)
+	if err != nil {
+		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
 	}
-	// handle PUT
-	if r.Method == http.MethodPut {
-		p.l.Println("PUT", r.URL.Path)
-		// expect the id in the URI
-		return
-	}
-	// catch all
-	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 // getProducts returns the products from the data store
-func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle GET Products")
+func (ph *ProductServiceHandler) getProductHandler(rw http.ResponseWriter, r *http.Request) {
 	// fetch the products from the datastore
 	lp := data.GetProducts()
 	// serialize the list to JSON
@@ -45,3 +30,47 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
 }
+
+func (ph *ProductServiceHandler) addProductHandler(rw http.ResponseWriter, r *http.Request) {
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+
+}
+
+func (ph *ProductServiceHandler) updateProductHandler(rw http.ResponseWriter, r *http.Request) {
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+	var id int
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
+
+
+}
+// ServeHttp is the main entry point for the handler and satisfies the http.Handler interface
+// interface
+func (p *ProductServiceHandler) ServeAPI(endpoint string) error {
+	// handle GET
+	handler := &ProductServiceHandler{}
+	r := mux.NewRouter()
+	ProductServiceHandler := r.PathPrefix("/products").Subrouter()
+	ProductServiceHandler.Methods("GET").Path("").HandlerFunc(handler.NewProductServiceHandler)
+	ProductServiceHandler.Methods("POST").Path("").HandlerFunc(handler.addProductHandler)
+	ProductServiceHandler.Methods("PUT").Path("").HandlerFunc(handler.updateProductHandler)
+	ProductServiceHandler.Methods("GET").Path("/{id:[0-9]+}").HandlerFunc(handler.getProductHandler)
+	return http.ListenAndServe(endpoint, r)
+}
+
